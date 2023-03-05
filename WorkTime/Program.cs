1eeb3148e242b0973a012b7edfc9fd9f43fc844a
YuseinToslev <jusein.toslev.highschool@svetlina.net>
2023-03-05 22:25:51 +0200

    up

---
+++ WorkTime/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace WorkTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            string d = Console.ReadLine();
+
+            switch (d)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+
+                    if (h >= 10 && h <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    break;
+
+                case "Sunday":
+
+               if (h >= 0)
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+
+            }
+
+        }
+    }
+}
