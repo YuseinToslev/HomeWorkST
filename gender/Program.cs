c0e8ecee8f0d94d401fa753f8344c91585c372fc
YuseinToslev <jusein.toslev.highschool@svetlina.net>
2023-03-05 22:37:53 +0200

    up

---
+++ gender/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics;
+
+namespace gender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gen = Console.ReadLine();
+
+            switch (gen)
+            {
+                case "m":
+                    if (age < 16)
+                    {
+                        Console.WriteLine("Master");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mr.");
+                    }
+                    break;
+                case "f":
+                    if (age < 16)
+                    {
+                        Console.WriteLine("Miss");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ms.");
+                    }
+                    break;
+            }
+        }
+    }
+}
