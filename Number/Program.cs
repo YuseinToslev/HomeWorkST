5eb81f02686bb33af6a4788578c7c80bc91e274a
YuseinToslev <jusein.toslev.highschool@svetlina.net>
2023-03-05 22:29:50 +0200

    up

---
+++ Number/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+
+            if (a > -100 && a < 100 && a != 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
